0b26094472ff6c6daed24ea74744796ea55bfeea
Daniela Caus <danicaus.br@gmail.com>
2021-07-11 22:33:38 -0300

    Create card component

---
+++ matching-game/src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function CardGame() {
+    return `
+        <article class="card-game">
+            <img src="images/logoAlura.png">
+        </article>
+    `;
+}
+
+export default CardGame;
